c97ddcc5d4ca87c9f64c02780b6f784b80284ec8
Mohit Bansal <mohit.bansal@walmart.com>
2019-04-05 18:34:20 -0700

    Adding Product Class

---
+++ src/main/java/com/example/demo/ProductClass.java
@@ -0,0 +1,98 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class ProductClass {
+
+    private String productId;
+
+    private String productName;
+
+    private String shortDescription;
+
+    private String longDescription;
+
+    private Float price;
+
+    private String productImage;
+
+    private int reviewRating;
+
+    private int reviewCount;
+
+    private boolean inStock;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public int getReviewRating() {
+        return reviewRating;
+    }
+
+    public void setReviewRating(int reviewRating) {
+        this.reviewRating = reviewRating;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+}
